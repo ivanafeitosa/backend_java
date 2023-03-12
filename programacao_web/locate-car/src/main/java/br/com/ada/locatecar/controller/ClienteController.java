447d34e36d718f54acc0f0cbbfe70942a8283854
Ivana Feitosa <ivsfeitosa@gmail.com>
2023-03-12 17:19:47 -0300

    finalizando o CRUD

--- programacao_web/locate-car/src/main/java/br/com/ada/locatecar/controller/ClienteController.java
+++ programacao_web/locate-car/src/main/java/br/com/ada/locatecar/controller/ClienteController.java
@@ -6,12 +6,10 @@ import br.com.ada.locatecar.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ClienteController {
@@ -45,6 +43,20 @@ public class ClienteController {
 
     }
 
+    @GetMapping("/cliente/{clienteId}/edit")
+    public String editarCliente(Model model, @PathVariable("clienteId") Long clienteId) {
+        Optional<Cliente> optionalCliente = this.clienteService.buscarClientePorId(clienteId);
+        optionalCliente.ifPresent(cliente -> model.addAttribute("cliente", cliente));
+        model.addAttribute("add", Boolean.FALSE);
+        return "cliente-add";
+    }
 
+    @PutMapping("/cliente/{clienteId}/edit")
+    public String editarCliente(@ModelAttribute("cliente") Cliente cliente, @PathVariable("clienteId") Long clienteId) {
+        cliente.setId(clienteId);
+        this.clienteService.createCliente(cliente);
+        return "redirect:/clientes";
+    }
 
 }
+
